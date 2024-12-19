0a867058dfc8c8d9c6ffe35ed466fdb92dd9dcef
Jose <josegarciajaime02@gmail.com>
2024-12-19 15:38:10 -0500

    update home page

--- routes/web.php
+++ routes/web.php
@@ -7,14 +7,15 @@ Route::middleware('guest')->group(function () {
     Route::redirect('/', '/login');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/home', function () {
+    return view('home');
+})->middleware(['auth', 'verified'])->name('home');
 
 Route::middleware('auth')->group(function () {
-    Route::redirect('/', '/dashboard');
+    Route::redirect('/', '/home');
 
     Route::get('/cocktails', [CocktailController::class, 'index'])->name('cocktails');
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
